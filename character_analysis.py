a21ebc405abc530104de3110540ca9a3eb50df83
Andre Ehrlich <andreehrlich@Andres-MacBook.local>
2023-09-22 16:29:38 +0300

    hello world

---
+++ character_analysis.py
@@ -0,0 +1,38 @@
+class Character:
+
+    def __init__():
+
+        self.name = name
+        self.nicknames = nicknames
+        self.relevant_lines = {}
+        # self.
+
+
+
+class TextLine:
+    def __init__():
+
+        pass 
+
+characters = {
+    "Oblonsky",
+    "Anna", 
+}
+
+
+book = {
+    "chapters": [],
+}
+
+# split by white space and full stops  
+for line in lines:
+    if any(w in line for w in ['was','is', 'will be']):
+
+
+inter_paragraph_floating_context = {}
+
+
+
+# phrase = subject verb object 
+
+# parts of speech:  noun, pronoun, verb, adjective, adverb, preposition, conjunction, and interjection.
